367b49c156c5ce0b208a9bbe8403c4c74f9bc8ae
Ricardo Pádua <ricardo@192.168.1.11>
2024-10-15 12:15:59 -0300

    automated script for install zsh + Oh My Zsh + PowerLevel9k

---
+++ zsh/uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Revert default shell to bash
+echo "Changing default shell back to bash..."
+chsh -s $(which bash)
+
+# Remove Oh My Zsh
+echo "Removing Oh My Zsh..."
+rm -rf $HOME/.oh-my-zsh
+
+# Remove Zsh configuration
+echo "Removing .zshrc..."
+rm -rf $HOME/.zshrc
+
+# Remove Zsh
+echo "Removing Zsh..."
+sudo dnf remove zsh fzf -y
+
+# Remove Zsh configuration
+echo "Removing .fonts..."
+rm -rf $HOME/.fonts/*
+fc-cache -fv
+
+echo "Uninstallation completed! You may need to restart your terminal or log out and log back in."
+
