3ea18c5da97e5cbc836e8d1424d48aa5a6a73e21
Ricardo Pádua <ricardo.padua4@gmail.com>
2024-10-15 17:29:52 -0300

    Update install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Extract and install nvim config
-if [ -f "nvim/.init.lua" ]; then
-    echo "move config..."
-    cp -rfvp nvim/init.lua 	 ~/.config/nvim/
-    cp -rfvp nvim/lua 		 ~/.config/nvim/
-    cp -rfvp nvim/plugin		 ~/.config/nvim/
-    echo "Neovim custom installed."
-fi
+echo "move config..."
+cp -rfvp nvim	 ~/.config/
+echo "Neovim custom installed."
 
 echo "Installation completed! You may need to restart your terminal or log out and log back in."
 
