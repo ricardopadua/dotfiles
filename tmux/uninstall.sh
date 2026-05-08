624efdcfa0e8196f8920be3a652ed810052c933b
ricardopadua <ricardo.padua4@gmail.com>
2026-05-07 21:37:14 -0300

    remove duplicate scripts

--- tmux/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Remove Tmux
-echo "Removing Tmux..."
-rm -rf $HOME/.tmux.conf
-sudo dnf remove tmux
-
-echo "Uninstallation completed! You may need to restart your terminal or log out and log back in."
-
